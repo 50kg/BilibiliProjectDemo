6735f0714dacb70cfc527cf8987ca67ae588e52f
50kg <sanji1.dev@gmail.com>
2017-02-24 16:56:08 +0800

    updata BaseUrl

--- app/src/main/java/com/example/sanji/bibiliproject/network/BaseUrl.java
+++ app/src/main/java/com/example/sanji/bibiliproject/network/BaseUrl.java
@@ -2,9 +2,11 @@ package com.example.sanji.bibiliproject.network;
 
 /**
  * Created by sanji on 2017/2/19.
+ * BaseUrl
  */
 
 public class BaseUrl {
+
     //http://live.bilibili.com/AppNewIndex/common?_device=android&platform=android&scale=xxhdpi
     public static final String BASE_URL_LIVE = "http://live.bilibili.com/";
 
@@ -13,5 +15,7 @@ public class BaseUrl {
 
     //http://api.m.panda.tv/index.php?method=category.list&type=game
     public static final String BASE_URL_PANDER_GAME = "http://api.m.panda.tv/";
+
+
 
 }
